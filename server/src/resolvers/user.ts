29920fc7415bd824adaa1bc08edcab70d00962f7
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2021-02-14 15:06:11 +0530

    Add authentication to the application

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,6 +1,6 @@
 import { User } from '../entities/User';
 import { MyContext } from '../types';
-import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver } from 'type-graphql';
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from 'type-graphql';
 import argon2 from 'argon2';
 
 @InputType()
@@ -30,6 +30,16 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+	@Query(() => User, { nullable: true })
+	async me(@Ctx() { req, em }: MyContext) {
+		// Query to check who the user is
+		if (!(req.session as any).userId) {
+			return null;
+		}
+		const user = await em.findOne(User, { id: (req.session as any).userId });
+		return user;
+	}
+
 	@Mutation(() => UserResponse)
 	async register(
 		@Arg('options') options: UserNamePasswordInput,
@@ -62,7 +72,7 @@ export class UserResolver {
 	@Mutation(() => UserResponse)
 	async login(
 		@Arg('options') options: UserNamePasswordInput,
-		@Ctx() { em }: MyContext
+		@Ctx() { em, req }: MyContext
 	): Promise<UserResponse> {
 		const user = await em.findOne(User, { username: options.username });
 		if (!user) {
@@ -73,6 +83,7 @@ export class UserResolver {
 			return { errors: [{ field: 'password', message: `Invalid password` }] };
 		}
 		await em.persistAndFlush(user!);
+		(req.session as any).userId = user.id;
 		return { user: user! };
 	}
 }
