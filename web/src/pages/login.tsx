1e99c8db3653d785b69b85d543c46242f57db917
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2021-02-14 22:50:54 +0530

    Add graphql queries and login page

---
+++ web/src/pages/login.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Form, Formik } from 'formik';
+import Wrapper from '../components/Wrapper';
+import InputField from '../components/InputField';
+import { Box, Button } from '@chakra-ui/react';
+import { useLoginMutation } from '../generated/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
+import { useRouter } from 'next/dist/client/router';
+
+interface loginProps {}
+
+export const Login: React.FC<loginProps> = ({}) => {
+	const [, login] = useLoginMutation();
+	const router = useRouter();
+	return (
+		<Wrapper variant='small'>
+			<Formik
+				initialValues={{ username: '', password: '' }}
+				onSubmit={async (values, { setErrors }) => {
+					const respone = await login({ options: values });
+					if (respone.data?.login.errors) {
+						setErrors(toErrorMap(respone.data.login.errors));
+					} else if (respone.data?.login.user) {
+						// worked
+						router.push('/');
+					}
+				}}
+			>
+				{({ isSubmitting }) => (
+					<Form>
+						<InputField
+							name='username'
+							placeholder='Username'
+							label='Username'
+						></InputField>
+						<Box mt={4}>
+							<InputField
+								name='password'
+								placeholder='Password'
+								label='Password'
+								type='password'
+							></InputField>
+						</Box>
+						<Button
+							mt={4}
+							type='submit'
+							colorScheme='teal'
+							isLoading={isSubmitting}
+						>
+							Login
+						</Button>
+					</Form>
+				)}
+			</Formik>
+		</Wrapper>
+	);
+};
+
+export default Login;
