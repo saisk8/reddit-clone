db2828c2eb5cb6d268cbf231bd62f809283cbcd1
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2021-02-15 12:24:50 +0530

    Add logout mutation

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,7 +1,17 @@
 import { User } from '../entities/User';
 import { MyContext } from '../types';
-import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from 'type-graphql';
+import {
+	Arg,
+	Ctx,
+	Field,
+	InputType,
+	Mutation,
+	ObjectType,
+	Query,
+	Resolver,
+} from 'type-graphql';
 import argon2 from 'argon2';
+import { COOKIE_NAME } from '../constants';
 
 @InputType()
 class UserNamePasswordInput {
@@ -47,23 +57,38 @@ export class UserResolver {
 	): Promise<UserResponse> {
 		if (options.username.length <= 2) {
 			return {
-				errors: [{ field: 'username', message: 'Username should have at least 3 characters' }],
+				errors: [
+					{
+						field: 'username',
+						message: 'Username should have at least 3 characters',
+					},
+				],
 			};
 		}
 
 		if (options.password.length <= 2) {
 			return {
-				errors: [{ field: 'password', message: 'Password should have at least 3 characters' }],
+				errors: [
+					{
+						field: 'password',
+						message: 'Password should have at least 3 characters',
+					},
+				],
 			};
 		}
 		const hashedPassword = await argon2.hash(options.password);
-		const user = em.create(User, { username: options.username, password: hashedPassword });
+		const user = em.create(User, {
+			username: options.username,
+			password: hashedPassword,
+		});
 		try {
 			await em.persistAndFlush(user);
 		} catch (err) {
 			if (err.code === '23505') {
 				// Duplicate user error
-				return { errors: [{ field: 'username', message: 'Username already exists' }] };
+				return {
+					errors: [{ field: 'username', message: 'Username already exists' }],
+				};
 			}
 		}
 		return { user };
@@ -76,14 +101,33 @@ export class UserResolver {
 	): Promise<UserResponse> {
 		const user = await em.findOne(User, { username: options.username });
 		if (!user) {
-			return { errors: [{ field: 'username', message: `Username does not exist` }] };
+			return {
+				errors: [{ field: 'username', message: `Username does not exist` }],
+			};
 		}
-		const isValidPassword = await argon2.verify(user!.password, options.password);
+		const isValidPassword = await argon2.verify(
+			user!.password,
+			options.password
+		);
 		if (!isValidPassword) {
 			return { errors: [{ field: 'password', message: `Invalid password` }] };
 		}
 		await em.persistAndFlush(user!);
 		(req.session as any).userId = user.id;
 		return { user: user! };
+	}
+
+	@Mutation(() => Boolean)
+	logout(@Ctx() { req, res }: MyContext) {
+		return new Promise((resolve) =>
+			req.session.destroy((err) => {
+				res.clearCookie(COOKIE_NAME);
+				if (err) {
+					resolve(false);
+					return;
+				}
+				resolve(true);
+			})
+		);
 	}
 }
