bae2651a95a3e4ed30dc12c54a9d0744a2d893f4
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2021-02-15 12:52:13 +0530

    Refactor code

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,80 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../theme';
 import { AppProps } from 'next/app';
-import { createClient, dedupExchange, fetchExchange, Provider } from 'urql';
-import { Cache, cacheExchange, QueryInput } from '@urql/exchange-graphcache';
-import {
-	LoginMutation,
-	MeDocument,
-	MeQuery,
-	RegisterMutation,
-} from '../generated/graphql';
-
-function betterUpdateQuery<Result, Query>(
-	cache: Cache,
-	queryInput: QueryInput,
-	result: any,
-	fn: (res: Result, query: Query) => Query
-) {
-	cache.updateQuery(queryInput, (data) => fn(result, data as any) as any);
-}
-
-const client = createClient({
-	url: 'http://localhost:4000/graphql',
-	fetchOptions: {
-		credentials: 'include',
-	},
-	exchanges: [
-		dedupExchange,
-		cacheExchange({
-			updates: {
-				Mutation: {
-					login: (_result, _, cache, __) => {
-						betterUpdateQuery<LoginMutation, MeQuery>(
-							cache,
-							{ query: MeDocument },
-							_result,
-							(result, query) => {
-								if (result.login.errors) {
-									return query;
-								} else {
-									return {
-										me: result.login.user,
-									};
-								}
-							}
-						);
-					},
-					register: (_result, _, cache, __) => {
-						betterUpdateQuery<RegisterMutation, MeQuery>(
-							cache,
-							{ query: MeDocument },
-							_result,
-							(result, query) => {
-								if (result.register.errors) {
-									return query;
-								} else {
-									return {
-										me: result.register.user,
-									};
-								}
-							}
-						);
-					},
-				},
-			},
-		}),
-		fetchExchange,
-	],
-});
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<Provider value={client}>
-			<ChakraProvider resetCSS theme={theme}>
-				<Component {...pageProps} />
-			</ChakraProvider>
-		</Provider>
+		<ChakraProvider resetCSS theme={theme}>
+			<Component {...pageProps} />
+		</ChakraProvider>
 	);
 }
 
