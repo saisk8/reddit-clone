59f682001505b67eaa6777deb4a2ec8058bb4923
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2021-02-15 11:57:45 +0530

    Add a Navbar

---
+++ web/src/components/Navbar.tsx
@@ -0,0 +1,45 @@
+import { Box, Button, Flex, Link } from '@chakra-ui/react';
+import React from 'react';
+import NextLink from 'next/link';
+import { useMeQuery } from '../generated/graphql';
+
+interface NavbarProps {}
+
+export const Navbar: React.FC<NavbarProps> = ({}) => {
+	const [{ data, fetching }] = useMeQuery();
+	let body = null;
+
+	if (fetching) {
+		// data is loading
+	} else if (!data?.me) {
+		// User not logged in
+		body = (
+			<>
+				<NextLink href='/login'>
+					<Link>Login</Link>
+				</NextLink>
+				<NextLink href='/register'>
+					<Link ml={4}>Register</Link>
+				</NextLink>
+			</>
+		);
+	} else {
+		// User logged in
+		body = (
+			<Box>
+				{data.me.username}
+				<Button variant='link' ml={2} colorScheme='red'>
+					Logout
+				</Button>
+			</Box>
+		);
+	}
+	return (
+		<Flex bgColor='gray.200'>
+			<Box p={4} ml='auto'>
+				{body}
+			</Box>
+		</Flex>
+	);
+};
+export default Navbar;
