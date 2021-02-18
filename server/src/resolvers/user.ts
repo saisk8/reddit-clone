bb14c49f130f5ae03954c0a9e2f71d3272d88372
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2021-02-18 14:25:51 +0530

    Delete the user from the cache

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -52,8 +52,8 @@ export class UserResolver {
 				],
 			};
 		}
-
-		const userId = await redis.get(FORGET_PASSWORD_PREFIX + token);
+		const key = FORGET_PASSWORD_PREFIX + token;
+		const userId = await redis.get(key);
 		if (!userId) {
 			return {
 				errors: [
@@ -80,7 +80,9 @@ export class UserResolver {
 		// Update password
 		user.password = await argon2.hash(newPassword);
 		await em.persistAndFlush(user);
+		await redis.del(key);
 
+		// Log in the user
 		(req.session as any).userId = userId;
 
 		return { user };
