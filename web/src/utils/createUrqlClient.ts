49e84224da5bb691ba206264f2d5beff7ffc2de7
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2021-02-24 13:12:33 +0530

    feat(exchnage): Add global error handling

--- web/src/utils/createUrqlClient.ts
+++ web/src/utils/createUrqlClient.ts
@@ -8,6 +8,20 @@ import {
 	RegisterMutation,
 } from '../generated/graphql';
 import { betterUpdateQuery } from './betterUpdateQuery';
+import { pipe, tap } from 'wonka';
+import { Exchange } from 'urql';
+import Router from 'next/router';
+
+const errorExchange: Exchange = ({ forward }) => (ops$) => {
+	return pipe(
+		forward(ops$),
+		tap(({ error }) => {
+			if (error?.message.includes('not authenticated')) {
+				Router.replace('/login');
+			}
+		})
+	);
+};
 
 export const createUrqlClient = (ssrExchange: any) => ({
 	url: 'http://localhost:4000/graphql',
@@ -62,6 +76,7 @@ export const createUrqlClient = (ssrExchange: any) => ({
 				},
 			},
 		}),
+		errorExchange,
 		ssrExchange,
 		fetchExchange,
 	],
