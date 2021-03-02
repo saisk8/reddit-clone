a20bc720f42f98bba546ce3cc4102030d7dac60d
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2021-03-02 21:02:12 +0530

    feat(posts): Add painated, styled posts to the main screen

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,4 +1,12 @@
-import { Link } from '@chakra-ui/react';
+import {
+	Link,
+	Stack,
+	Box,
+	Heading,
+	Text,
+	Flex,
+	Button,
+} from '@chakra-ui/react';
 import { withUrqlClient } from 'next-urql';
 import NextLink from 'next/link';
 import Layout from '../components/Layout';
@@ -6,22 +14,44 @@ import { usePostsQuery } from '../generated/graphql';
 import { createUrqlClient } from '../utils/createUrqlClient';
 
 const Index = () => {
-	const [{ data }] = usePostsQuery({
+	const [{ data, fetching }] = usePostsQuery({
 		variables: {
 			limit: 10,
 		},
 	});
+
+	if (!fetching && !data) {
+		return <div>You got no posts for some reason.</div>;
+	}
+
 	return (
 		<Layout>
-			<NextLink href='/create-post'>
-				<Link>Create post</Link>
-			</NextLink>{' '}
+			<Flex align='center'>
+				<Heading>Reddit Clone</Heading>
+				<NextLink href='/create-post'>
+					<Link ml='auto'>Create post</Link>
+				</NextLink>{' '}
+			</Flex>
 			<br />
-			{!data ? (
+			{!data && fetching ? (
 				<div>Loading...</div>
 			) : (
-				data?.posts.map((p) => <div key={p.id}>{p.title}</div>)
+				<Stack spacing={8}>
+					{data?.posts.map((p) => (
+						<Box key={p.id} p={5} shadow='md' borderWidth='1px'>
+							<Heading fontSize='xl'>{p.title}</Heading>
+							<Text mt={4}>{p.textSnippet}</Text>
+						</Box>
+					))}
+				</Stack>
 			)}
+			{data ? (
+				<Flex>
+					<Button isLoading={fetching} m='auto' my={8}>
+						Load more
+					</Button>
+				</Flex>
+			) : null}
 		</Layout>
 	);
 };
